ffe46aeb770a6cb55c772e7ac84c84b7cf188d9c
varghese <varghese87es@gmail.com>
2020-08-10 12:45:17 +0530

    update on nodemailer settings

--- index.js
+++ index.js
@@ -131,16 +131,17 @@ app.post("/register", (req, res) => {
                                         //console.log(response)
                                         //console.log(req.body.email)
                                         let transporter = nodemailer.createTransport({
-                                            host: "smtp.gmail.com",
-                                            port: 587,
-                                            secure: false,
+                                            // host: "smtp.gmail.com",
+                                            // port: 587,
+                                            // secure: false,
+                                            service:'gmail',
                                             auth: {
                                                 user: 'varghese87joseph@gmail.com',
                                                 pass: process.env.PASSWORD
                                             },
-                                            tls: {
-                                                rejectUnauthorized: false
-                                            }
+                                            // tls: {
+                                            //     rejectUnauthorized: false
+                                            // }
                                         });
                                         let mailOptions = {
                                             from: 'varghese87joseph@gmail.com',
